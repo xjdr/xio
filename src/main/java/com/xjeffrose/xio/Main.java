7ab3ae212677e2303af7ab7102411d18577e105a
jr <jeff.rose@getbraintree.com>
2015-03-23 00:24:10 -0500

    Added Travis

--- src/main/java/com/xjeffrose/xio/Main.java
+++ src/main/java/com/xjeffrose/xio/Main.java
@@ -30,11 +30,6 @@ class Main {
 
       /* Client c = new Client(); */
       /* Future<HttpResponse> f = c.get(8000); */
-      /* s.serve(8080, 8); */
-      /* s.serve("localhost",8080); */
-      /* s.serve("localhost",8080, 8); */
-      /* s.serve(127.0.0.1,8080); */
-      /* serve(127.0.0.1,8080,8); */
 
       // Await.ready(s.serve()); => all async and stuff
 
