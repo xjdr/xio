412ce347de865cb5de2eb71b772d4884ccd58c36
Jeff Rose <jefrose@paypal.com>
2015-08-24 10:44:11 -0500

    fix port binding collisions

--- src/test/java/com/xjeffrose/xio/guice/XioModuleTest.java
+++ src/test/java/com/xjeffrose/xio/guice/XioModuleTest.java
@@ -49,7 +49,7 @@ public class XioModuleTest {
                 .limitConnectionsTo(200)
                 .limitFrameSizeTo(1024)
                 .limitQueuedResponsesPerConnection(50)
-                .listen(new InetSocketAddress(8083))
+                .listen(new InetSocketAddress(8084))
 //        .listen(new InetSocketAddress("127.0.0.1", 8082))
                 .name("Xio Test Server")
                 .taskTimeout(new Duration((double) 20000, TimeUnit.MILLISECONDS))
