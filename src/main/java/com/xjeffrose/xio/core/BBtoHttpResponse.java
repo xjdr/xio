e1bb3e1339f7282c74cf801d30adf282ff257d7e
Xun Lai <xlai@paypal.com>
2015-10-12 13:59:23 -0700

    removed the toString in BBtoHttpResponse and keep the body as it (especially for gzip)

--- src/main/java/com/xjeffrose/xio/core/BBtoHttpResponse.java
+++ src/main/java/com/xjeffrose/xio/core/BBtoHttpResponse.java
@@ -2,6 +2,7 @@ package com.xjeffrose.xio.core;
 
 import com.google.common.base.Joiner;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufProcessor;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.DefaultFullHttpResponse;
@@ -13,7 +14,8 @@ import javax.annotation.Nullable;
 
 public class BBtoHttpResponse {
 
-  private BBtoHttpResponse() { }
+  private BBtoHttpResponse() {
+  }
 
   public static DefaultFullHttpResponse getResponse(ByteBuf byteBuf) {
     return BBtoHttpResponse.getResponse(null, byteBuf);
@@ -25,28 +27,41 @@ public class BBtoHttpResponse {
     final Joiner joiner = Joiner.on(" ").skipNulls();
     DefaultFullHttpResponse httpResponse;
 
-    String[] headerBody = response.toString(Charset.defaultCharset()).split("\r\n\r\n");
-    String[] headers = headerBody[0].split("\r\n");
+    String[] headers = null;
+    ByteBuf body = null;
+    String delimiter = "\r\n\r\n";
+    ByteBufProcessor processor = new MultiBytesFieldSplitProcessor(delimiter.getBytes());
+    int delimIdx = response.forEachByte(0, response.readableBytes(), processor);
+    if (delimIdx < 0) {
+      headers = response.toString(Charset.defaultCharset()).split("\r\n");
+      body = null;
+    } else {
+      ByteBuf h = response.slice(0, delimIdx - delimiter.length() + 1);
+      headers = h.toString(Charset.defaultCharset()).split("\r\n");
+      delimIdx++; // move to start of body
+      body = response.slice(delimIdx, byteBuf.readableBytes() - delimIdx);
+    }
+
     String[] firstLine = headers[0].split("\\s");
 
     // Lets make a HTTP Response object now
     if (ctx == null) {
-      if (headerBody.length > 1) {
+      if (body != null) {
         httpResponse = new DefaultFullHttpResponse(
             HttpVersion.valueOf(firstLine[0]),
             new HttpResponseStatus(Integer.parseInt(firstLine[1]), joiner.join(Arrays.copyOfRange(firstLine, 2, 5))),
-            Unpooled.wrappedBuffer(headerBody[1].getBytes()));
+            Unpooled.wrappedBuffer(body));
       } else {
         httpResponse = new DefaultFullHttpResponse(
             HttpVersion.valueOf(firstLine[0]),
             new HttpResponseStatus(Integer.parseInt(firstLine[1]), joiner.join(Arrays.copyOfRange(firstLine, 2, 5))));
       }
     } else {
-      if (headerBody.length > 1) {
+      if (body != null) {
         httpResponse = new DefaultFullHttpResponse(
             HttpVersion.valueOf(firstLine[0]),
             new HttpResponseStatus(Integer.parseInt(firstLine[1]), joiner.join(Arrays.copyOfRange(firstLine, 2, 5))),
-            ctx.alloc().buffer().writeBytes(headerBody[1].getBytes()));
+            ctx.alloc().buffer().writeBytes(body));
       } else {
         httpResponse = new DefaultFullHttpResponse(
             HttpVersion.valueOf(firstLine[0]),
@@ -60,5 +75,29 @@ public class BBtoHttpResponse {
     }
 
     return httpResponse;
+  }
+
+  static class MultiBytesFieldSplitProcessor implements ByteBufProcessor {
+
+    private final byte[] delimiter;
+    private int index;
+
+    public MultiBytesFieldSplitProcessor(byte[] recordDelimiterByte) {
+      this.delimiter = recordDelimiterByte;
+    }
+
+    @Override
+    public boolean process(byte value) throws Exception {
+      if (delimiter[index] != value) {
+        index = 0;
+        return true;
+      }
+      if (index != delimiter.length - 1) {
+        index++;
+        return true;
+      }
+      index = 0;
+      return false;
+    }
   }
 }
