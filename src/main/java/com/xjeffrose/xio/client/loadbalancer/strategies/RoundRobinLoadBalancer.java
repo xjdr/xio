13625d5d37e2a3428757595867719175358b21f4
Xun Lai <xlai@paypal.com>
2016-04-08 14:18:44 -0700

    empty node list check for load balance stragegy

--- src/main/java/com/xjeffrose/xio/client/loadbalancer/strategies/RoundRobinLoadBalancer.java
+++ src/main/java/com/xjeffrose/xio/client/loadbalancer/strategies/RoundRobinLoadBalancer.java
@@ -3,7 +3,6 @@ package com.xjeffrose.xio.client.loadbalancer.strategies;
 import com.google.common.collect.ImmutableList;
 import com.xjeffrose.xio.client.loadbalancer.Node;
 import com.xjeffrose.xio.client.loadbalancer.Strategy;
-import java.util.Vector;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class RoundRobinLoadBalancer implements Strategy {
@@ -12,6 +11,10 @@ public class RoundRobinLoadBalancer implements Strategy {
 
   @Override
   public Node getNextNode(ImmutableList<Node> pool) {
+    if (pool.isEmpty()) {
+      return null;
+    }
+
     if (last.get() == pool.size()) {
       last.set(0);
       return pool.get(0);
