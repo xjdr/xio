d3edaa699f7a5994f5fe0eb603db0b26178c8446
xjdr <jeff.rose12@gmail.com>
2016-06-09 17:25:54 -0500

    create listener and requisite exceptions

--- src/main/java/com/xjeffrose/xio/client/XioListener.java
+++ src/main/java/com/xjeffrose/xio/client/XioListener.java
@@ -1,5 +1,22 @@
 package com.xjeffrose.xio.client;
 
-public interface XioListener {
+import java.util.UUID;
 
+public interface XioListener<T> {
+
+  void onRequestSent();
+
+  void onResponseReceived(T message, boolean success);
+
+  void onChannelError(Exception requestException) throws XioClientException;
+
+  T getResponse(UUID id) throws XioClientTimeoutException;
+
+  boolean getStatus(UUID id) throws XioClientTimeoutException;
+
+  void onChannelReadComplete();
+
+  void addID(UUID id);
+
+  void removeID(UUID id);
 }
