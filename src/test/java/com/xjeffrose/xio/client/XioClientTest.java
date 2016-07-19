5d92cda89e44328c4ce82a612a7de4c0eaa3477a
Madan, Smarth <madan.smarth@gmail.com>
2016-07-19 11:44:04 -0700

    Adding a flag for nodeHealthCheck to distributor

--- src/test/java/com/xjeffrose/xio/client/XioClientTest.java
+++ src/test/java/com/xjeffrose/xio/client/XioClientTest.java
@@ -298,7 +298,7 @@ public class XioClientTest {
 
     final RoundRobinLoadBalancer strategy = new RoundRobinLoadBalancer();
     final ImmutableList<Node> pool = ImmutableList.of(new Node(new InetSocketAddress("127.0.0.1", 9110)), new Node(new InetSocketAddress("127.0.0.1", 9120)), new Node(new InetSocketAddress("127.0.0.1", 9130)));
-    final Distributor distributor = new Distributor(pool, strategy, new NodeHealthCheck(2));
+    final Distributor distributor = new Distributor(pool, strategy, new NodeHealthCheck(2), "dummyName");
 
     XioClient xioClient = new XioClient();
     ListenableFuture<XioClientChannel> responseFuture = xioClient.connectAsync(new TcpClientConnector(distributor.pick().address()),  new BoundedExponentialBackoffRetry(1000, 100000, 3));
