9d4891d52a6b89f39198b205d2910a3253ed93af
Chris Keating <christopherkeating@gmail.com>
2016-08-19 15:33:16 -0500

    Log zookeeper write errors

--- src/main/java/com/xjeffrose/xio/storage/ZooKeeperWriteProvider.java
+++ src/main/java/com/xjeffrose/xio/storage/ZooKeeperWriteProvider.java
@@ -1,8 +1,10 @@
 package com.xjeffrose.xio.storage;
 
 import com.xjeffrose.xio.marshall.Marshaller;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
 
+@Slf4j
 public class ZooKeeperWriteProvider extends WriteProvider {
 
   private CuratorFramework client;
@@ -19,6 +21,7 @@ public class ZooKeeperWriteProvider extends WriteProvider {
     try {
       client.create().orSetData().creatingParentsIfNeeded().forPath(key, bytes);
     } catch (Exception e) {
+      log.error("ZooKeeperWriteProvider.write", e);
       throw new RuntimeException(e);
     }
   }
