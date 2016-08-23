647dfe166d582fa7e3b7a245d70d25f7501490e6
Chris Keating <christopherkeating@gmail.com>
2016-08-23 13:00:10 -0500

    Refactor dirty flag marking in Ruleset.

--- src/main/java/com/xjeffrose/xio/config/Ruleset.java
+++ src/main/java/com/xjeffrose/xio/config/Ruleset.java
@@ -8,8 +8,34 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode
 public class Ruleset {
 
-  private final IpAddressDeterministicRuleEngineConfig ipRules = new IpAddressDeterministicRuleEngineConfig();
-  boolean ipRulesDirty = false;
+  static public class Markable<T> {
+    private final T data;
+    private boolean marked = false;
+
+    Markable(T data) {
+      this.data = data;
+    }
+
+    public T mutate() {
+      marked = true;
+      return data;
+    }
+
+    public T get() {
+      return data;
+    }
+
+    public boolean marked() {
+      return marked;
+    }
+
+    public void clearMark() {
+      marked = false;
+    }
+  }
+
+  private final Markable<IpAddressDeterministicRuleEngineConfig> ipRules = new Markable<>(new IpAddressDeterministicRuleEngineConfig());
+  private final Markable<Http1DeterministicRuleEngineConfig> http1Rules = new Markable<>(new Http1DeterministicRuleEngineConfig());
 
   private final String ipFilterPath;
   private final String http1FilterPath;
@@ -22,21 +48,32 @@ public class Ruleset {
 
   public long write(WriteProvider writer) {
     long recordsWritten = 0;
-    if (ipRulesDirty) {
-      writer.write(ipFilterPath, ipRules);
-      recordsWritten += ipRules.size();
-      ipRulesDirty = false;
+
+    if (ipRules.marked()) {
+      writer.write(ipFilterPath, ipRules.get());
+      recordsWritten += ipRules.get().size();
+      ipRules.clearMark();
+    }
+
+    if (http1Rules.marked()) {
+      writer.write(http1FilterPath, http1Rules.get());
+      recordsWritten += http1Rules.get().size();
+      http1Rules.clearMark();
     }
 
     return recordsWritten;
   }
 
   public void read(ReadProvider reader) {
-    reader.read(ipFilterPath, ipRules);
+    reader.read(ipFilterPath, ipRules.get());
+    reader.read(http1FilterPath, http1Rules.get());
   }
 
   public IpAddressDeterministicRuleEngineConfig mutateIpRules() {
-    ipRulesDirty = true;
-    return ipRules;
+    return ipRules.mutate();
+  }
+
+  public Http1DeterministicRuleEngineConfig mutateHttpRules() {
+    return http1Rules.mutate();
   }
 }
