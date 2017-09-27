9101601bec8d45f9886fbe601dfdd6007c3bd66b
Chris Keating <christopherkeating@gmail.com>
2017-09-27 08:49:26 -0700

    Added GentleSslHandler

---
+++ src/main/java/com/xjeffrose/xio/handler/GentleSslHandler.java
@@ -0,0 +1,60 @@
+package com.xjeffrose.xio.handler;
+
+import com.google.common.base.Preconditions;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelHandler;
+import io.netty.handler.ssl.OptionalSslHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslHandler;
+
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * GentleSslHandler will attach an SSL handler to the pipeline if the incoming
+ * data is not clear text. Otherwise it will attach a clear text handler. It
+ * is expected that the clear text handler will inform the peer to try to
+ * communicate again over SSL, then close the connection.
+ */
+
+@Slf4j
+public class GentleSslHandler extends OptionalSslHandler {
+
+  private final ChannelHandler cleartextHandler;
+
+  /**
+   * cleartextHandler must be Sharable
+   */
+  public GentleSslHandler(SslContext sslContext, ChannelHandlerAdapter cleartextHandler) {
+    super(sslContext);
+    Preconditions.checkArgument(cleartextHandler.isSharable(), "cleartextHandler must be Sharable");
+    this.cleartextHandler = cleartextHandler;
+  }
+
+  @Override
+  protected String newSslHandlerName() {
+    return "ssl handler";
+  }
+
+  /*
+  @Override
+  protected SslHandler newSslHandler(ChannelHandlerContext context, SslContext sslContext) {
+    System.out.println("new ssl");
+    return sslContext.newHandler(context.alloc());
+  }
+  */
+
+  @Override
+  protected String newNonSslHandlerName() {
+    return "cleartext upgrade handler";
+  }
+
+  @Override
+  protected ChannelHandler newNonSslHandler(ChannelHandlerContext context) {
+    return cleartextHandler;
+  }
+}
---
+++ src/main/java/com/xjeffrose/xio/http/Recipes.java
@@ -0,0 +1,199 @@
+package com.xjeffrose.xio.http;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.DefaultHttpRequest;
+import io.netty.handler.codec.http.DefaultHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequest;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpRequestEncoder;
+import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.HttpResponseDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpVersion;
+import java.nio.charset.StandardCharsets;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+public final class Recipes {
+  protected Recipes() {}
+
+  private static final HttpVersion v1_1 = HttpVersion.HTTP_1_1;
+
+  public static enum ContentType {
+    Application_Json("application/json"),
+    Text_Plain("text/plain"),
+    Text_Html("text/html");
+
+    private final String value;
+
+    ContentType(String value) {
+      this.value = value;
+    }
+
+  }
+
+  public static ByteBuf unpooledBuffer(String payload) {
+    return Unpooled.copiedBuffer(payload.getBytes(StandardCharsets.UTF_8));
+  }
+
+  // Request {{{
+  public static FullHttpRequest newFullRequest(HttpMethod method, String urlPath, ByteBuf buffer, ContentType contentType) {
+    FullHttpRequest request = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, method, urlPath, buffer);
+    request.headers().set(CONTENT_TYPE, contentType.value);
+    request.headers().setInt(CONTENT_LENGTH, buffer.readableBytes());
+    return request;
+  }
+  public static HttpRequest newRequestDelete(String urlPath) {
+    return new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.DELETE, urlPath);
+  }
+
+  public static HttpRequest newRequestGet(String urlPath) {
+    return new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, urlPath);
+  }
+
+  public static HttpRequest newRequestPost(String urlPath, ByteBuf buffer, ContentType contentType) {
+    return newFullRequest(HttpMethod.POST, urlPath, buffer, contentType);
+  }
+
+  public static HttpRequest newRequestPost(String urlPath, String payload, ContentType contentType) {
+    return newRequestPost(urlPath, unpooledBuffer(payload), contentType);
+  }
+
+  public static HttpRequest newRequestPut(String urlPath, ByteBuf buffer, ContentType contentType) {
+    return newFullRequest(HttpMethod.PUT, urlPath, buffer, contentType);
+  }
+
+  public static HttpRequest newRequestPut(String urlPath, String payload, ContentType contentType) {
+    return newRequestPut(urlPath, unpooledBuffer(payload), contentType);
+  }
+  // Request }}}
+
+  // Response {{{
+  public static HttpResponse newResponse(HttpResponseStatus status) {
+    return new DefaultHttpResponse(v1_1, status);
+  }
+
+  public static HttpResponse newResponse(HttpResponseStatus status, ByteBuf buffer, ContentType contentType) {
+    FullHttpResponse response = new DefaultFullHttpResponse(v1_1, status, buffer);
+
+    response.headers().set(CONTENT_TYPE, contentType.value);
+    response.headers().setInt(CONTENT_LENGTH, buffer.readableBytes());
+
+    return response;
+  }
+
+  public static HttpResponse newResponse(HttpResponseStatus status, String payload, ContentType contentType) {
+    return newResponse(status, unpooledBuffer(payload), contentType);
+  }
+
+  // OK {{{
+  public static HttpResponse newResponseOk() {
+    return newResponse(HttpResponseStatus.OK);
+  }
+
+  public static HttpResponse newResponseOk(String payload) {
+    return newResponse(HttpResponseStatus.OK, payload, ContentType.Text_Plain);
+  }
+
+  public static HttpResponse newResponseOk(String payload, ContentType contentType) {
+    return newResponse(HttpResponseStatus.OK, payload, contentType);
+  }
+  // OK }}}
+
+  // BAD_REQUEST {{{
+  public static HttpResponse newResponseBadRequest() {
+    return newResponse(HttpResponseStatus.BAD_REQUEST);
+  }
+
+  public static HttpResponse newResponseBadRequest(String payload) {
+    return newResponse(HttpResponseStatus.BAD_REQUEST, payload, ContentType.Text_Plain);
+  }
+
+  public static HttpResponse newResponseBadRequest(String payload, ContentType contentType) {
+    return newResponse(HttpResponseStatus.BAD_REQUEST, payload, contentType);
+  }
+  // BAD_REQUEST }}}
+  // Response }}}
+
+  // useful for both encoders and decoders
+  static public List<ByteBuf> extractBuffers(EmbeddedChannel channel) {
+    channel.runPendingTasks();
+    List<ByteBuf> buffers = Lists.newArrayList();
+    while (true) {
+      ByteBuf buffer = channel.readOutbound();
+      if (buffer == null) {
+        break;
+      }
+      buffers.add(buffer);
+    }
+    return buffers;
+  }
+
+  // encoders {{{
+  static public List<ByteBuf> encodeRequest(DefaultFullHttpRequest request) {
+    EmbeddedChannel channel = new EmbeddedChannel();
+
+    channel.pipeline().addLast("http request encoder", new HttpRequestEncoder());
+    channel.writeOutbound(request);
+    return extractBuffers(channel);
+  }
+
+  static public List<ByteBuf> encodeResponse(HttpResponse response) {
+    EmbeddedChannel channel = new EmbeddedChannel();
+
+    channel.pipeline().addLast("http response encoder", new HttpResponseEncoder());
+    channel.writeOutbound(response);
+    return extractBuffers(channel);
+  }
+  // encoders }}}
+
+  // decoders {{{
+
+  static public HttpRequest decodeRequest(List<ByteBuf> payload) {
+    EmbeddedChannel channel = new EmbeddedChannel();
+
+    channel.pipeline()
+      .addLast("http request decoder", new HttpRequestDecoder())
+      .addLast("http message aggregator", new HttpObjectAggregator(1048576))
+      ;
+
+    for (ByteBuf buffer : payload) {
+      channel.writeInbound(buffer);
+    }
+
+    HttpRequest request = channel.readInbound();
+    return request;
+  }
+
+  static public HttpResponse decodeResponse(List<ByteBuf> payload) {
+    EmbeddedChannel channel = new EmbeddedChannel();
+
+    channel.pipeline()
+      .addLast("http response decoder", new HttpResponseDecoder())
+      .addLast("http message aggregator", new HttpObjectAggregator(1048576))
+      ;
+
+    for (ByteBuf buffer : payload) {
+      channel.writeInbound(buffer);
+    }
+
+    HttpResponse response = channel.readInbound();
+    return response;
+  }
+  // decoders }}}
+
+}
