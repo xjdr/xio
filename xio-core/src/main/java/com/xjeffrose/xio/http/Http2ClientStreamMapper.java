bfdd7bb4b91ec376b885c9bf7f0d5f276b22211e
manimaul <manimaul@gmail.com>
2018-05-09 09:42:34 -0700

    TACO-20 recycling client b4 atomically setting available in pool

--- xio-core/src/main/java/com/xjeffrose/xio/http/Http2ClientStreamMapper.java
+++ xio-core/src/main/java/com/xjeffrose/xio/http/Http2ClientStreamMapper.java
@@ -5,7 +5,9 @@ import com.google.common.collect.HashBiMap;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http2.Http2Connection;
 import io.netty.util.AttributeKey;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Http2ClientStreamMapper {
 
   static final AttributeKey<Http2ClientStreamMapper> H2_CLIENT_STREAM_MAPPER =
@@ -28,6 +30,7 @@ public class Http2ClientStreamMapper {
     Integer mappedId = streamMap.get(id);
     if (mappedId == null) {
       mappedId = connection.local().incrementAndGetNextStreamId();
+      log.debug("h2 client outbound stream id {} : {}", id, mappedId);
       streamMap.put(id, mappedId);
     }
     return mappedId;
@@ -37,13 +40,16 @@ public class Http2ClientStreamMapper {
     Integer mappedId;
     if (remove) {
       mappedId = streamMap.inverse().remove(id);
+      log.debug("h2 client inbound stream id {} : {}", id, mappedId);
     } else {
       mappedId = streamMap.inverse().get(id);
+      log.debug("h2 client inbound stream id {} : {}", id, mappedId);
     }
     return mappedId == null ? id : mappedId;
   }
 
   public void clear() {
+    log.debug("h2 client stream mapping clear");
     streamMap.clear();
   }
 }
