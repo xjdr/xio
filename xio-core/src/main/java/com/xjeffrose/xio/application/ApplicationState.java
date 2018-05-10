ff12793f2b98778c3334fc9297f9bf5124a9c97b
Will Kamp <manimaul@gmail.com>
2018-05-10 11:48:44 -0700

    removing vestigial code (#233)
    * removing vestigial code
    
    * but wait there is more

--- xio-core/src/main/java/com/xjeffrose/xio/application/ApplicationState.java
+++ xio-core/src/main/java/com/xjeffrose/xio/application/ApplicationState.java
@@ -11,8 +11,6 @@ import com.xjeffrose.xio.core.ZkClient;
 import com.xjeffrose.xio.filter.Http1FilterConfig;
 import com.xjeffrose.xio.filter.IpFilterConfig;
 import com.xjeffrose.xio.http.ClientState;
-import com.xjeffrose.xio.http.DefaultRouter;
-import com.xjeffrose.xio.http.Router;
 import com.xjeffrose.xio.tracing.XioTracing;
 import io.netty.channel.EventLoopGroup;
 import java.util.concurrent.atomic.AtomicReference;
@@ -46,8 +44,6 @@ public class ApplicationState {
 
   private final AtomicReference<Http1FilterConfig> http1FilterConfig;
 
-  private final AtomicReference<Router> router;
-
   public ApplicationState(ApplicationConfig config) {
     this.config = config;
     this.tracing = config.getTracing();
@@ -65,11 +61,6 @@ public class ApplicationState {
     http1FilterConfig = new AtomicReference<>(new Http1FilterConfig());
     zkClient.registerUpdater(
         new Http1FilterConfig.Updater(config.getHttp1FilterPath(), this::setHttp1FilterConfig));
-
-    // Set this to null.  If router is being bootstrapped from here, it will check for null before
-    // proceeding.
-    // It is expected that dynamic configuration service will update this as needed.
-    router = new AtomicReference<>(new DefaultRouter());
   }
 
   public EventLoopGroup workerGroup() {
@@ -90,14 +81,6 @@ public class ApplicationState {
 
   public void setHttp1FilterConfig(Http1FilterConfig newConfig) {
     http1FilterConfig.set(newConfig);
-  }
-
-  public Router getRouter() {
-    return router.get();
-  }
-
-  public void setRouter(Router router) {
-    this.router.set(router);
   }
 
   public ClientState createClientState(
--- xio-core/src/main/java/com/xjeffrose/xio/core/XioRoutingFilterFactory.java
+++
@@ -1,7 +0,0 @@
-package com.xjeffrose.xio.core;
-
-import io.netty.channel.ChannelInboundHandler;
-
-public interface XioRoutingFilterFactory {
-  ChannelInboundHandler getRoutingFilter();
-}
