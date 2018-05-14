d22a9cbff240213b126e57baa95023420605dcf1
david.capino <david.capino@gmail.com>
2018-05-14 13:56:27 -0700

    parameterize more things

--- int-test-backend-server/scripts/spinup.sh
+++ int-test-backend-server/scripts/spinup.sh
@@ -2,13 +2,17 @@
 
 NAME=$1
 KEY=$2
+AMI=$3
+SG=$4
+PROFILE=$5
+SUBNET=$6
 
 # Create Launch Configuration
 aws autoscaling create-launch-configuration --launch-configuration-name $NAME \
-  --image-id ami-c00072b8 --key-name $KEY --security-groups sg-adadbed3 --instance-type t2.micro \
-  --region us-west-2 --profile nordstrom-federated
+  --image-id $AMI --key-name $KEY --security-groups $SG --instance-type t2.micro \
+  --region us-west-2 --profile $PROFILE
 
 # Spin up the ASG
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name $NAME \
   --launch-configuration-name $NAME --min-size 0 --max-size 0 \
-  --vpc-zone-identifier subnet-3bac8a60 --region us-west-2 --profile nordstrom-federated
+  --vpc-zone-identifier $SUBNET --region us-west-2 --profile $PROFILE
