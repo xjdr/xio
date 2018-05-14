d6cb911c186e0087bbcf544b79fcab7cf7c33712
david.capino <david.capino@gmail.com>
2018-05-14 13:46:42 -0700

    TACO-151 - Parameterize key name

--- int-test-backend-server/scripts/spinup.sh
+++ int-test-backend-server/scripts/spinup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 NAME=$1
+KEY=$2
 
 # Create Launch Configuration
 aws autoscaling create-launch-configuration --launch-configuration-name $NAME \
-  --image-id ami-c00072b8 --key-name kratos --security-groups sg-adadbed3 --instance-type t2.micro \
+  --image-id ami-c00072b8 --key-name $KEY --security-groups sg-adadbed3 --instance-type t2.micro \
   --region us-west-2 --profile nordstrom-federated
 
 # Spin up the ASG
