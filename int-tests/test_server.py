ef5698c0e7ed6e223e1ba0532eec55ff6cea2cb6
manimaul <manimaul@gmail.com>
2018-05-17 13:21:38 -0700

    TACO-231 proxy post integration tests

--- int-tests/test_server.py
+++ int-tests/test_server.py
@@ -1,11 +1,13 @@
 import unittest
-from unsafe_client import http_get
+from unsafe_client import http_get, http_post
 import os.path as path
 from server_controller import Server, Initializer, module_dir
 from unittest import TestCase, skip
 
 
 class TestReverseProxyServer(TestCase):
+
+  # region setup
 
   @classmethod
   def setUpClass(cls):
@@ -42,14 +44,29 @@ class TestReverseProxyServer(TestCase):
     self.front_end = Server(front_init.init_script, front_ready_str, conf, proxy_config,
                             name="proxy", verbose=False).run()
 
+  # endregion
+
   # region tests
 
   # @skip
-  def test_backend_server_works(self):
+  def test_backend_server_get_works(self):
     self.setup_back(h2=False)
     for each in self.back_ends:
-      response = http_get(url='https://localhost:{}/'.format(each.port))
-      self.assertEqual('backend1', response.headers['header-tag'])
+      response = http_get(url='https://localhost:{}/'.format(each.port), headers={"x-echo": "echo"})
+      self.assertEqual('backend1', response.headers['x-tag'])
+      self.assertEqual('GET', response.headers['x-method'])
+      self.assertEqual('echo', response.headers['x-echo'])
+      self.assertEqual('Release the Kraken', response.body)
+      self.assertEqual(200, response.status)
+
+  # @skip
+  def test_backend_server_post_works(self):
+    self.setup_back(h2=False)
+    for each in self.back_ends:
+      response = http_post(url='https://localhost:{}/'.format(each.port), data={"key": "value"}, headers={"x-echo": "echo"})
+      self.assertEqual('backend1', response.headers['x-tag'])
+      self.assertEqual('POST', response.headers['x-method'])
+      self.assertEqual('echo', response.headers['x-echo'])
       self.assertEqual('Release the Kraken', response.body)
       self.assertEqual(200, response.status)
 
@@ -57,25 +74,61 @@ class TestReverseProxyServer(TestCase):
   def test_proxy_get_h2_h1(self):
     self.setup_front(h2=True)
     self.setup_back(h2=False)
-    responses = (http_get(url='https://localhost:8443/', h2=True),
-                 # http_get(url='https://localhost:8443/', h2=True),  # todo: (WK) fails Http2To1ProxyRequestQueue
+    responses = (http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}),
+                 # http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}),  # todo: (WK) fails Http2To1ProxyRequestQueue
                  )
     for response in responses:
-      self.assertEqual('backend1', response.headers['header-tag'])
+      self.assertEqual('backend1', response.headers['x-tag'])
+      self.assertEqual('GET', response.headers['x-method'])
+      self.assertEqual('echo', response.headers['x-echo'])
       self.assertEqual('Release the Kraken', response.body)
       self.assertEqual(200, response.status)
 
   # @skip
+  def test_proxy_post_h2_h1(self):
+    self.setup_front(h2=True)
+    self.setup_back(h2=False)
+    responses = (http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}),
+                 # http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}),  # todo: (WK) fails Http2To1ProxyRequestQueue
+                 )
+    for response in responses:
+      self.assertEqual('backend1', response.headers['x-tag'])
+      self.assertEqual('POST', response.headers['x-method'])
+      self.assertEqual('echo', response.headers['x-echo'])
+      self.assertEqual('Release the Kraken', response.body)
+      self.assertEqual(200, response.status)
+
+  # @skip("fix me first")
   def test_proxy_get_h1_h1(self):
     self.setup_front(h2=False)
     self.setup_back(h2=False)
-    responses = (http_get(url='https://localhost:8443/'),
-                 # http_get(url='https://localhost:8443/'),  # todo: (WK) fails Http2To1ProxyRequestQueue
+    responses = (http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}),
+                 # http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}),  # todo: (WK) fails Http2To1ProxyRequestQueue
                  )
     for response in responses:
-      self.assertEqual('backend1', response.headers['header-tag'])
+      self.assertEqual('backend1', response.headers['x-tag'])
+      self.assertEqual('GET', response.headers['x-method'])
+      self.assertEqual('echo', response.headers['x-echo'])
       self.assertEqual('Release the Kraken', response.body)
       self.assertEqual(200, response.status)
+
+  # @skip("fix me first too")
+  def test_proxy_post_h1_h1(self):
+    self.setup_front(h2=False)
+    self.setup_back(h2=False)
+    responses = (http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}),
+                 # http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}),  # todo: (WK) fails Http2To1ProxyRequestQueue
+                 )
+    for response in responses:
+      self.assertEqual('backend1', response.headers['x-tag'])
+      self.assertEqual('POST', response.headers['x-method'])
+      self.assertEqual('echo', response.headers['x-echo'])
+      self.assertEqual('Release the Kraken', response.body)
+      self.assertEqual(200, response.status)
+
+  # todo: (WK) :
+  # test large POST
+  # test h2_h2
 
   # endregion
 
