ffd9076e4147850168a61b70d9a7f4f1806c03d3
manimaul <manimaul@gmail.com>
2018-05-22 18:05:50 -0700

    TACO-231 integration test h2 tests

--- int-tests/test_server.py
+++ int-tests/test_server.py
@@ -1,134 +1,162 @@
-import unittest
-from unsafe_client import http_get, http_post
 import os.path as path
+import unittest
+from unittest import TestCase
+
 from server_controller import Server, Initializer, module_dir
-from unittest import TestCase, skip
-import json
+from unsafe_client import http_get, http_post, Response
+
+back_init = Initializer('int-test-backend-server')
+front_init = Initializer('int-test-proxy-server')
+back_end = None
+front_end = None
 
 
 class TestReverseProxyServer(TestCase):
 
-  # region setup
-
   @classmethod
-  def setUpClass(cls):
-    global back_init
-    back_init = Initializer('int-test-backend-server')
-    global front_init
-    front_init = Initializer('int-test-proxy-server')
-
-  def setUp(self):
-    self.front_end = None
-    self.back_ends = []
-
-  def tearDown(self):
-    for each in [each for each in self.back_ends + [self.front_end] if each is not None]:
+  def tearDownClass(cls):
+    for each in [each for each in [back_end, front_end] if each is not None]:
       each.kill()
 
-  def setup_back(self, h2: bool):
+  @classmethod
+  def setup_back(cls, h2: bool, verbose=False):
     back_ready_str = "starting to accept connections"
-    self.back_ends += [Server(back_init.init_script, back_ready_str, h2,
-                              name="backend1", port=8444, verbose=False).run()
-                       ]
+    global back_end
+    back_end = Server(back_init.init_script, back_ready_str, h2, name="backend1", port=8444, verbose=verbose).run()
 
-  def setup_front(self, h2: bool):
+  @classmethod
+  def setup_front(cls, h2: bool, verbose=False):
     front_ready_str = "proxy accepting connections"
     conf = path.abspath(path.join(module_dir, "proxy.conf"))
-    self.assertTrue(path.exists(conf))
     if h2:
       proxy_config = 'xio.h2ReverseProxy'
     else:
       proxy_config = 'xio.h1ReverseProxy'
-    self.front_end = Server(front_init.init_script, front_ready_str, conf, proxy_config,
-                            name="proxy", verbose=False).run()
+    global front_end
+    front_end = Server(front_init.init_script, front_ready_str, conf, proxy_config, name="proxy", verbose=verbose).run()
 
-  # endregion
+  def check_response(self, response: Response, method: str):
+    self.assertEqual('backend1', response.headers['x-tag'])
+    self.assertEqual(method, response.headers['x-method'])
+    self.assertEqual('echo', response.headers['x-echo'])
+    self.assertEqual({'title': 'Release', 'description': 'the Kraken'}, response.json_body)
+    self.assertEqual(200, response.status)
 
-  # region tests
 
-  # @skip
-  def test_backend_server_get_works(self):
-    self.setup_back(h2=False)
-    for each in self.back_ends:
-      response = http_get(url='https://localhost:{}/'.format(each.port), headers={"x-echo": "echo"})
-      self.assertEqual('backend1', response.headers['x-tag'])
-      self.assertEqual('GET', response.headers['x-method'])
-      self.assertEqual('echo', response.headers['x-echo'])
-      self.assertEqual({'title': 'Release', 'description': 'the Kraken'}, response.json_body)
-      self.assertEqual(200, response.status)
-
-  # @skip
-  def test_backend_server_post_works(self):
-    self.setup_back(h2=False)
-    for each in self.back_ends:
-      response = http_post(url='https://localhost:{}/'.format(each.port), data={"key": "value"}, headers={"x-echo": "echo"})
-      self.assertEqual('backend1', response.headers['x-tag'])
-      self.assertEqual('POST', response.headers['x-method'])
-      self.assertEqual('echo', response.headers['x-echo'])
-      self.assertEqual({'title': 'Release', 'description': 'the Kraken'}, response.json_body)
-      self.assertEqual(200, response.status)
+class TestReverseProxyServerH1H1(TestReverseProxyServer):
+  @classmethod
+  def setUpClass(cls):
+    print("setup h1:h1")
+    cls.setup_front(h2=False)
+    cls.setup_back(h2=False)
 
   # @skip
-  def test_proxy_get_h2_h1(self):
-    self.setup_front(h2=True)
-    self.setup_back(h2=False)
-    responses = (http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}),
-                 http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}),
-                 )
-    for response in responses:
-      self.assertEqual('backend1', response.headers['x-tag'])
-      self.assertEqual('GET', response.headers['x-method'])
-      self.assertEqual('echo', response.headers['x-echo'])
-      self.assertEqual({'title': 'Release', 'description': 'the Kraken'}, response.json_body)
-      self.assertEqual(200, response.status)
+  def test_backend_server_get_h1_works(self):
+    response = http_get(url='https://localhost:{}/'.format(back_end.port), headers={"x-echo": "echo"}, h2=False)
+    self.check_response(response, 'GET')
 
   # @skip
-  def test_proxy_post_h2_h1(self):
-    self.setup_front(h2=True)
-    self.setup_back(h2=False)
-    responses = (http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}),
-                 http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}),
-                 )
-    for response in responses:
-      self.assertEqual('backend1', response.headers['x-tag'])
-      self.assertEqual('POST', response.headers['x-method'])
-      self.assertEqual('echo', response.headers['x-echo'])
-      self.assertEqual({'title': 'Release', 'description': 'the Kraken'}, response.json_body)
-      self.assertEqual(200, response.status)
+  def test_backend_server_post_h1_works(self):
+    response = http_post(url='https://localhost:{}/'.format(back_end.port), data={"key": "value"},
+                         headers={"x-echo": "echo"}, h2=False)
+    self.check_response(response, 'POST')
 
   # @skip
   def test_proxy_get_h1_h1(self):
-    self.setup_front(h2=False)
-    self.setup_back(h2=False)
-    responses = (http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}),
-                 http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}),
-                 )
+    responses = [
+      http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}, h2=False),
+      http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}, h2=False),
+    ]
     for response in responses:
-      self.assertEqual('backend1', response.headers['x-tag'])
-      self.assertEqual('GET', response.headers['x-method'])
-      self.assertEqual('echo', response.headers['x-echo'])
-      self.assertEqual({'title': 'Release', 'description': 'the Kraken'}, response.json_body)
-      self.assertEqual(200, response.status)
+      self.check_response(response, 'GET')
 
   # @skip
   def test_proxy_post_h1_h1(self):
-    self.setup_front(h2=False)
-    self.setup_back(h2=False)
-    responses = (http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}),
-                 http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}),
-                 )
+    responses = [
+      http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}, h2=False),
+      http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}, h2=False),
+    ]
     for response in responses:
-      self.assertEqual('backend1', response.headers['x-tag'])
-      self.assertEqual('POST', response.headers['x-method'])
-      self.assertEqual('echo', response.headers['x-echo'])
-      self.assertEqual({'title': 'Release', 'description': 'the Kraken'}, response.json_body)
-      self.assertEqual(200, response.status)
+      self.check_response(response, 'POST')
+
+
+class TestReverseProxyServerH2H1(TestReverseProxyServer):
+
+  @classmethod
+  def setUpClass(cls):
+    print("setup h2:h1")
+    cls.setup_front(h2=True)
+    cls.setup_back(h2=False)
+
+  # @skip
+  def test_proxy_get_h2_h1(self):
+    responses = [
+      http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}, h2=True),
+      http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}, h2=True),
+    ]
+    for response in responses:
+      self.check_response(response, 'GET')
+
+  # @skip
+  def test_proxy_post_h2_h1(self):
+    responses = [
+      http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}, h2=True),
+      http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}, h2=True),
+    ]
+    for response in responses:
+      self.check_response(response, 'POST')
+
+
+class TestReverseProxyServerH2H2(TestReverseProxyServer):
+
+  @classmethod
+  def setUpClass(cls):
+    print("setup h2:h2")
+    cls.setup_front(h2=True)
+    cls.setup_back(h2=True)
+
+  # @skip
+  def test_backend_server_get_h2_works(self):
+    responses = [
+      http_get(url='https://localhost:{}/'.format(back_end.port), headers={"x-echo": "echo"}, h2=True),
+      http_get(url='https://localhost:{}/'.format(back_end.port), headers={"x-echo": "echo"}, h2=True),
+    ]
+    for response in responses:
+      self.check_response(response, 'GET')
+
+  # @skip
+  def test_backend_server_post_h2_works(self):
+    responses = [
+      http_post(url='https://localhost:{}/'.format(back_end.port), data={"key": "value"}, headers={"x-echo": "echo"},
+                h2=True),
+      http_post(url='https://localhost:{}/'.format(back_end.port), data={"key": "value"}, headers={"x-echo": "echo"},
+                h2=True),
+    ]
+
+    for response in responses:
+      self.check_response(response, 'POST')
+
+  # @skip
+  def test_proxy_get_h2_h2(self):
+    responses = [
+      http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}, h2=True),
+      http_get(url='https://localhost:8443/', headers={"x-echo": "echo"}, h2=True),
+    ]
+    for response in responses:
+      self.check_response(response, 'GET')
+
+  # @skip
+  def test_proxy_post_h2_h2(self):
+    responses = [
+      http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}, h2=True),
+      http_post(url='https://localhost:8443/', data={"key": "value"}, headers={"x-echo": "echo"}, h2=True),
+    ]
+    for response in responses:
+      self.check_response(response, 'POST')
 
   # todo: (WK) :
   # test large POST
-  # test h2_h2
-
-  # endregion
+  # wrk2 and h2load load tests
 
 
 if __name__ == '__main__':
--- int-tests/unsafe_client.py
+++ int-tests/unsafe_client.py
@@ -2,6 +2,9 @@ from io import BytesIO
 import pycurl
 from urllib.parse import urlencode
 import json
+import requests
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class Response(object):
   def __init__(self):
@@ -15,8 +18,9 @@ class Response(object):
 
 
 _CHAR_ENCODING = 'iso-8859-1'
+_TIMEOUT_SECS = 5
 
-def _prep_request(url: str, headers: dict):
+def _prep_request(url: str, headers: dict, verbose: bool):
   response = Response()
 
   def _header_function(header_line):
@@ -32,6 +36,7 @@ def _prep_request(url: str, headers: dict):
 
   buffer = BytesIO()
   curl = pycurl.Curl()
+  curl.reset()
   curl.setopt(pycurl.URL, url)
   if headers != None:
     headers = ["{}: {}".format(str(each[0]), str(each[1])) for each in headers.items()]
@@ -39,10 +44,12 @@ def _prep_request(url: str, headers: dict):
     pass
   curl.setopt(pycurl.WRITEDATA, buffer)
   curl.setopt(pycurl.HTTP_VERSION, pycurl.CURL_HTTP_VERSION_2_0)
+  if verbose:
+    curl.setopt(curl.VERBOSE, True)
   curl.setopt(pycurl.SSL_VERIFYPEER, 0)
   curl.setopt(pycurl.SSL_VERIFYHOST, 0)
   curl.setopt(pycurl.HEADERFUNCTION, _header_function)
-  curl.setopt(pycurl.TIMEOUT, 2)
+  curl.setopt(pycurl.TIMEOUT, _TIMEOUT_SECS)
   return curl, response, buffer
 
 
@@ -53,15 +60,32 @@ def _complete_request(curl: pycurl.Curl, buffer: BytesIO, response: Response):
   curl.close()
 
 
-def http_get(url: str, headers: dict = None):
-  curl, response, buffer = _prep_request(url, headers)
-  _complete_request(curl, buffer, response)
-  return response
+def http_get(url: str, headers: dict = None, h2: bool = False, verbose: bool = False):
+  if h2:
+    curl, response, buffer = _prep_request(url, headers, verbose)
+    _complete_request(curl, buffer, response)
+    return response
+  else:
+    r = requests.get(url, headers=headers, verify=False, timeout=_TIMEOUT_SECS)
+    response = Response()
+    response.headers = r.headers
+    response.body = r.text
+    response.status = r.status_code
+    return response
 
 
-def http_post(url: str, data: dict, headers: dict= None):
-  curl, response, buffer = _prep_request(url, headers)
-  postfields = urlencode(data)
-  curl.setopt(pycurl.POSTFIELDS, postfields)
-  _complete_request(curl, buffer, response)
-  return response
+
+def http_post(url: str, data: dict, headers: dict= None, h2: bool = False, verbose: bool = False):
+  if h2:
+    curl, response, buffer = _prep_request(url, headers, verbose)
+    postfields = urlencode(data)
+    curl.setopt(pycurl.POSTFIELDS, postfields)
+    _complete_request(curl, buffer, response)
+    return response
+  else:
+    r = requests.post(url, headers=headers, data=data, verify=False, timeout=_TIMEOUT_SECS)
+    response = Response()
+    response.headers = r.headers
+    response.body = r.text
+    response.status = r.status_code
+    return response
