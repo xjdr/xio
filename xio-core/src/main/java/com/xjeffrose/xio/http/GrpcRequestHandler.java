557d6a47604f7142db5379b809cec60456f72821
Brian Radebaugh <brian.radebaugh@nordstrom.com>
2018-06-19 16:54:47 -0700

    added lots of protection for around indicated size for gRPC requests, fixed gRPC responses

--- xio-core/src/main/java/com/xjeffrose/xio/http/GrpcRequestHandler.java
+++ xio-core/src/main/java/com/xjeffrose/xio/http/GrpcRequestHandler.java
@@ -2,16 +2,55 @@ package com.xjeffrose.xio.http;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.buffer.UnpooledByteBufAllocator;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.*;
 import io.netty.util.AttributeKey;
 
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.function.Function;
 
+// Documentation for gRPC over HTTP2: https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-HTTP2.md
+
 public class GrpcRequestHandler<GrpcRequest extends
   com.google.protobuf.GeneratedMessageV3, GrpcResponse extends com.google.protobuf.GeneratedMessageV3> implements PipelineRequestHandler {
+
+  private static final String GRPC_TRAILING_HEADER_STATUS_KEY = "grpc-status";
+  private static final String GRPC_CONTENT_TYPE_VALUE = "application/grpc+proto";
+
+  private enum GrpcStatus { // These are defined by gRPC
+    OK("0"),
+    CANCELLED("1"),
+    UNKNOWN("2"),
+    INVALID_ARGUMENT("3"),
+    DEADLINE_EXCEEDED("4"),
+    NOT_FOUND("5"),
+    ALREADY_EXISTS("6"),
+    PERMISSION_DENIED("7"),
+    RESOURCE_EXHAUSTED("8"),
+    FAILED_PRECONDITION("9"),
+    ABORTED("10"),
+    OUT_OF_RANGE("11"),
+    UNIMPLEMENTED("12"),
+    INTERNAL("13"),
+    UNAVAILABLE("14"),
+    DATA_LOSS("15"),
+    UNAUTHENTICATED("16");
+
+    private final String text;
+
+    GrpcStatus(final String text) {
+      this.text = text;
+    }
+
+    @Override
+    public String toString() {
+      return text;
+    }
+  }
+
   private static class GrpcState {
     int size;
     ByteBuf buffer;
@@ -27,10 +66,10 @@ public class GrpcRequestHandler<GrpcRequest extends
 
   private static final int sizeTooLargeToHandle = 1_000_000;
 
-  static HashMap<Integer, GrpcState> lazyCreateSession(ChannelHandlerContext ctx) {
+  private static HashMap<Integer, GrpcState> lazyCreateSession(ChannelHandlerContext ctx) {
     HashMap<Integer, GrpcState> session = ctx.channel().attr(CHANNEL_MESSAGE_SESSION_KEY).get();
     if (session == null) {
-      session = new HashMap<Integer, GrpcState>();
+      session = new HashMap<>();
       ctx.channel().attr(CHANNEL_MESSAGE_SESSION_KEY).set(session);
     }
     return session;
@@ -39,7 +78,6 @@ public class GrpcRequestHandler<GrpcRequest extends
   private final GrpcRequestParser<GrpcRequest> requestParser;
   private final Function<GrpcRequest, GrpcResponse> appLogic;
 
-  // TODO brian: make this pretty, be able to pass in HelloRequest::parseFrom
   GrpcRequestHandler(GrpcRequestParser<GrpcRequest> requestParser, Function<GrpcRequest, GrpcResponse> appLogic) {
     this.requestParser = requestParser;
     this.appLogic = appLogic;
@@ -63,45 +101,99 @@ public class GrpcRequestHandler<GrpcRequest extends
     }
 
     if (state == null) {
-      int size = actualBuffer.slice(1, 4).readInt();
+      boolean haveMetaData = actualBuffer.isReadable(5);
+      if (!haveMetaData) {
+        // TODO: Chris, not sure how we want to handle scenarios where we don't get the entire metadata in the first packet
+        sendResponse(ctx, request.streamId(), Unpooled.EMPTY_BUFFER, GrpcStatus.INTERNAL);
+        return;
+      }
 
+      boolean isCompressed = actualBuffer.slice(0, 1).readBoolean();
+      if (isCompressed) {
+        sendResponse(ctx, request.streamId(), Unpooled.EMPTY_BUFFER, GrpcStatus.UNIMPLEMENTED);
+        return;
+      }
+
+      int size = actualBuffer.slice(1, 4).readInt();
       if (size >= sizeTooLargeToHandle) {
-        System.out.println("total data is too big");
-        // TODO: return appropriate error and end connection.
+        // TODO: Chris, how do we shut down this request so we don't recieve future packets
+        sendResponse(ctx, request.streamId(), Unpooled.EMPTY_BUFFER, GrpcStatus.RESOURCE_EXHAUSTED);
         return;
       }
 
       int packetContentSize = actualBuffer.readableBytes() - 5;
+
+      boolean firstChunkIsLargerThanIndicatedSize = packetContentSize > size;
+      if (firstChunkIsLargerThanIndicatedSize) {
+        // TODO: Chris, how do we shut down this request so we don't recieve future packets
+        sendResponse(ctx, request.streamId(), Unpooled.EMPTY_BUFFER, GrpcStatus.INTERNAL);
+        return;
+      }
+
       ByteBuf contentBuffer = UnpooledByteBufAllocator.DEFAULT.buffer(packetContentSize, size);
       contentBuffer.writeBytes(actualBuffer.slice(5, packetContentSize));
 
       state = new GrpcState(size, contentBuffer);
     } else {
+      boolean accumulatedIsLargerThanIndicatedSize = state.buffer.readableBytes() + actualBuffer.readableBytes() > state.size;
+      if (accumulatedIsLargerThanIndicatedSize) {
+        // TODO: Chris, how do we shut down this request so we don't recieve future packets
+        sendResponse(ctx, request.streamId(), Unpooled.EMPTY_BUFFER, GrpcStatus.INTERNAL);
+        return;
+      }
+
       state.buffer.writeBytes(actualBuffer);
     }
 
-    session.put(request.streamId(), state);
-
     if (request.endOfMessage()) {
-      if (state.size != state.buffer.readableBytes()) {
-        // TODO: return appropriate error and end connection.
-        System.out.println("the expected length and the actual length do not match");
-        return;
-      }
-
-      try {
-        ByteBuf responseBuffer = makeResponseBuffer(state.buffer.nioBuffer());
-        ctx.writeAndFlush(responseBuffer);
-
-      } catch (InvalidProtocolBufferException e) {
-        // TODO: return appropriate error and end connection.
-        System.out.println("not able to create request object from request bytes");
-        return;
-      }
+      handleGrpcRequest(ctx, state, request.streamId());
     } else {
-      // save the state until we have the end of message
       session.put(request.streamId(), state);
     }
+  }
+
+  private void handleGrpcRequest(ChannelHandlerContext ctx, GrpcState state, int streamId) {
+    if (state.size != state.buffer.readableBytes()) {
+      // TODO: Chris, how do we shut down this request so we don't recieve future packets
+      sendResponse(ctx, streamId, Unpooled.EMPTY_BUFFER, GrpcStatus.INTERNAL);
+      return;
+    }
+
+    try {
+      ByteBuf grpcResponseBuffer = makeResponseBuffer(state.buffer.nioBuffer());
+      sendResponse(ctx, streamId, grpcResponseBuffer, GrpcStatus.OK);
+    } catch (InvalidProtocolBufferException e) {
+      sendResponse(ctx, streamId, Unpooled.EMPTY_BUFFER, GrpcStatus.INTERNAL);
+    }
+  }
+
+  private void sendResponse(ChannelHandlerContext ctx, int streamId, ByteBuf grpcResponseBuffer, GrpcStatus status) {
+    Headers headers = new DefaultHeaders()
+      .set(HttpHeaderNames.CONTENT_TYPE, GRPC_CONTENT_TYPE_VALUE);
+    DefaultSegmentedResponse segmentedResponse = DefaultSegmentedResponse
+      .builder()
+      .status(HttpResponseStatus.OK)
+      .streamId(streamId)
+      .headers(headers)
+      .build();
+
+    ctx.writeAndFlush(segmentedResponse);
+
+    // TODO: need to add status-message
+    Headers trailingHeaders = new DefaultHeaders()
+      .set(GRPC_TRAILING_HEADER_STATUS_KEY, status.toString());
+    DefaultSegmentedData data = DefaultSegmentedData
+      .builder()
+      .content(grpcResponseBuffer)
+      .trailingHeaders(trailingHeaders)
+      .endOfMessage(true)
+      .build();
+
+    ctx.writeAndFlush(data);
+
+    // TODO: Chris, I assume we need to remove the state at this point
+    HashMap<Integer, GrpcState> session = lazyCreateSession(ctx);
+    session.remove(streamId);
   }
 
   private ByteBuf makeResponseBuffer(ByteBuffer requestBuffer) throws InvalidProtocolBufferException {
--- xio-core/src/test/java/com/xjeffrose/xio/http/GrpcRequestHandlerTest.java
+++ xio-core/src/test/java/com/xjeffrose/xio/http/GrpcRequestHandlerTest.java
@@ -7,13 +7,15 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.UnpooledByteBufAllocator;
 import io.netty.channel.*;
 import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.net.SocketAddress;
 import java.nio.ByteBuffer;
+import java.util.Objects;
 
 public class GrpcRequestHandlerTest extends Assert {
 
@@ -21,11 +23,232 @@ public class GrpcRequestHandlerTest extends Assert {
   private GrpcRequestHandler subject;
   private static final String responsePrefix = "I'm a response: ";
 
-  private ByteBuf bufferFor(com.google.protobuf.GeneratedMessageV3 protoObject) {
+  @Before
+  public void setUp() {
+    subject = new GrpcRequestHandler<>(HelloRequest::parseFrom, (HelloRequest request) -> HelloReply
+      .newBuilder()
+      .setMessage(responsePrefix + request.getName())
+      .build());
+
+    channel = new EmbeddedChannel(new SimpleChannelInboundHandler<Request>() {
+      @Override
+      protected void channelRead0(ChannelHandlerContext ctx, Request request) {
+        subject.handle(ctx, request, null);
+      }
+    });
+  }
+
+  @Test
+  public void testSimpleRequest() {
+    HelloRequest grpcRequest = HelloRequest.newBuilder().setName("myName").build();
+    ByteBuf grpcRequestBuffer = bufferFor(grpcRequest);
+    int streamId = 123;
+
+    SegmentedRequestData segmentedRequest = fullGrpcRequest(grpcRequestBuffer, streamId,true);
+    channel.writeInbound(segmentedRequest);
+
+    Response response = channel.readOutbound();
+    SegmentedData segmentedData = channel.readOutbound();
+
+    assertEquals(HttpResponseStatus.OK, response.status());
+    assertEquals(streamId, response.streamId());
+    assertEquals("application/grpc+proto", response.headers().get(HttpHeaderNames.CONTENT_TYPE));
+
+    HelloReply actualReply = protoObjectFor(segmentedData.content(), HelloReply::parseFrom);
+    HelloReply expectedReply = HelloReply.newBuilder().setMessage(responsePrefix + grpcRequest.getName()).build();
+    assertEquals(actualReply, expectedReply);
+
+    assertEquals("0", Objects.requireNonNull(segmentedData.trailingHeaders()).get("grpc-status"));
+    assertTrue(segmentedData.endOfMessage());
+  }
+
+  @Test
+  public void testChunkedRequest() {
+    HelloRequest grpcRequest = HelloRequest.newBuilder().setName("myName").build();
+    ByteBuf grpcRequestBuffer = bufferFor(grpcRequest);
+    int streamId = 234;
+
+    int middleIndex = grpcRequestBuffer.readableBytes() / 2;
+    ByteBuf firstHalf = grpcRequestBuffer.slice(0, middleIndex);
+    ByteBuf secondHalf = grpcRequestBuffer.slice(middleIndex, grpcRequestBuffer.readableBytes() - middleIndex);
+
+    channel.writeInbound(fullGrpcRequest(firstHalf, streamId, false));
+    channel.writeInbound(fullGrpcRequest(secondHalf, streamId, true));
+
+    Response response = channel.readOutbound();
+    SegmentedData segmentedData = channel.readOutbound();
+
+    assertEquals(HttpResponseStatus.OK, response.status());
+    assertEquals(streamId, response.streamId());
+    assertEquals("application/grpc+proto", response.headers().get(HttpHeaderNames.CONTENT_TYPE));
+
+    HelloReply actualReply = protoObjectFor(segmentedData.content(), HelloReply::parseFrom);
+    HelloReply expectedReply = HelloReply.newBuilder().setMessage(responsePrefix + grpcRequest.getName()).build();
+    assertEquals(actualReply, expectedReply);
+
+    assertEquals("0", Objects.requireNonNull(segmentedData.trailingHeaders()).get("grpc-status"));
+    assertTrue(segmentedData.endOfMessage());
+  }
+
+  @Test
+  public void testCompressedFlag() {
+    HelloRequest grpcRequest = HelloRequest.newBuilder().setName("myName").build();
+    ByteBuf grpcRequestBuffer = bufferFor(grpcRequest, true);
+    int streamId = 345;
+
+    SegmentedRequestData segmentedRequest = fullGrpcRequest(grpcRequestBuffer, streamId, true);
+    channel.writeInbound(segmentedRequest);
+
+    Response response = channel.readOutbound();
+    SegmentedData segmentedData = channel.readOutbound();
+
+    assertEquals(HttpResponseStatus.OK, response.status());
+    assertEquals(streamId, response.streamId());
+    assertEquals("application/grpc+proto", response.headers().get(HttpHeaderNames.CONTENT_TYPE));
+
+    assertEquals("12", Objects.requireNonNull(segmentedData.trailingHeaders()).get("grpc-status"));
+    assertTrue(segmentedData.endOfMessage());
+  }
+
+  @Test
+  public void testIndicatedSizeTooLarge() {
+    byte[] lengthByteBuffer = ByteBuffer.allocate(4).putInt(1_000_001).array();
+    byte[] compressedByteBuffer = ByteBuffer.allocate(1).put((byte)0).array();
+    int streamId = 345;
+
+    ByteBuf grpcRequestBuffer = UnpooledByteBufAllocator.DEFAULT.buffer(5, 5);
+    grpcRequestBuffer.writeBytes(compressedByteBuffer);
+    grpcRequestBuffer.writeBytes(lengthByteBuffer);
+
+    channel.writeInbound(fullGrpcRequest(grpcRequestBuffer, streamId, true));
+
+    Response response = channel.readOutbound();
+    SegmentedData segmentedData = channel.readOutbound();
+
+    assertEquals(HttpResponseStatus.OK, response.status());
+    assertEquals(streamId, response.streamId());
+    assertEquals("application/grpc+proto", response.headers().get(HttpHeaderNames.CONTENT_TYPE));
+
+    assertEquals("8", Objects.requireNonNull(segmentedData.trailingHeaders()).get("grpc-status"));
+    assertTrue(segmentedData.endOfMessage());
+  }
+
+  @Test
+  public void testDidNotGetAllMetaData() {
+    byte[] lengthByteBuffer = ByteBuffer.allocate(4).putInt(0).array();
+    int streamId = 456;
+
+    ByteBuf grpcRequestBuffer = UnpooledByteBufAllocator.DEFAULT.buffer(4, 4);
+    grpcRequestBuffer.writeBytes(lengthByteBuffer);
+
+    channel.writeInbound(fullGrpcRequest(grpcRequestBuffer, streamId, true));
+
+    Response response = channel.readOutbound();
+    SegmentedData segmentedData = channel.readOutbound();
+
+    assertEquals(HttpResponseStatus.OK, response.status());
+    assertEquals(streamId, response.streamId());
+    assertEquals("application/grpc+proto", response.headers().get(HttpHeaderNames.CONTENT_TYPE));
+
+    assertEquals("13", Objects.requireNonNull(segmentedData.trailingHeaders()).get("grpc-status"));
+    assertTrue(segmentedData.endOfMessage());
+  }
+
+  @Test
+  public void testIndicatedSizeSmallerThanActualSizeSingleChunk() {
+    HelloRequest grpcRequest = HelloRequest.newBuilder().setName("myName").build();
+    byte[] dataBytes = grpcRequest.toByteArray();
+    byte[] lengthByteBuffer = ByteBuffer.allocate(4).putInt(2).array();
+    byte[] compressedByteBuffer = ByteBuffer.allocate(1).put((byte)0).array();
+    int streamId = 567;
+
+    int length = dataBytes.length;
+    ByteBuf grpcRequestBuffer = UnpooledByteBufAllocator.DEFAULT.buffer(length + 5, length + 5);
+
+    grpcRequestBuffer.writeBytes(compressedByteBuffer);
+    grpcRequestBuffer.writeBytes(lengthByteBuffer);
+    grpcRequestBuffer.writeBytes(dataBytes);
+
+    channel.writeInbound(fullGrpcRequest(grpcRequestBuffer, streamId, true));
+
+    Response response = channel.readOutbound();
+    SegmentedData segmentedData = channel.readOutbound();
+
+    assertEquals(HttpResponseStatus.OK, response.status());
+    assertEquals(streamId, response.streamId());
+    assertEquals("application/grpc+proto", response.headers().get(HttpHeaderNames.CONTENT_TYPE));
+
+    assertEquals("13", Objects.requireNonNull(segmentedData.trailingHeaders()).get("grpc-status"));
+    assertTrue(segmentedData.endOfMessage());
+  }
+
+  @Test
+  public void testIndicatedSizeSmallerThanActualSizeMultipleChunks() {
+    HelloRequest grpcRequest = HelloRequest.newBuilder().setName("myName").build();
+    byte[] dataBytes = grpcRequest.toByteArray();
+    byte[] lengthByteBuffer = ByteBuffer.allocate(4).putInt(6).array();
+    byte[] compressedByteBuffer = ByteBuffer.allocate(1).put((byte)0).array();
+    int streamId = 567;
+
+    int length = dataBytes.length;
+    ByteBuf grpcRequestBuffer = UnpooledByteBufAllocator.DEFAULT.buffer(length + 5, length + 5);
+
+    grpcRequestBuffer.writeBytes(compressedByteBuffer);
+    grpcRequestBuffer.writeBytes(lengthByteBuffer);
+    grpcRequestBuffer.writeBytes(dataBytes);
+
+    int middleIndex = grpcRequestBuffer.readableBytes() / 2;
+    ByteBuf firstHalf = grpcRequestBuffer.slice(0, middleIndex);
+    ByteBuf secondHalf = grpcRequestBuffer.slice(middleIndex, grpcRequestBuffer.readableBytes() - middleIndex);
+
+    channel.writeInbound(fullGrpcRequest(firstHalf, streamId, false));
+    channel.writeInbound(fullGrpcRequest(secondHalf, streamId, true));
+
+    Response response = channel.readOutbound();
+    SegmentedData segmentedData = channel.readOutbound();
+
+    assertEquals(HttpResponseStatus.OK, response.status());
+    assertEquals(streamId, response.streamId());
+    assertEquals("application/grpc+proto", response.headers().get(HttpHeaderNames.CONTENT_TYPE));
+
+    assertEquals("13", Objects.requireNonNull(segmentedData.trailingHeaders()).get("grpc-status"));
+    assertTrue(segmentedData.endOfMessage());
+  }
+
+  @Test
+  public void testIndicatedSizeLargerThanActualSize() {
+    HelloRequest grpcRequest = HelloRequest.newBuilder().setName("myName").build();
+    byte[] dataBytes = grpcRequest.toByteArray();
+    byte[] lengthByteBuffer = ByteBuffer.allocate(4).putInt(900_000).array();
+    byte[] compressedByteBuffer = ByteBuffer.allocate(1).put((byte)0).array();
+    int streamId = 567;
+
+    int length = dataBytes.length;
+    ByteBuf grpcRequestBuffer = UnpooledByteBufAllocator.DEFAULT.buffer(length + 5, length + 5);
+
+    grpcRequestBuffer.writeBytes(compressedByteBuffer);
+    grpcRequestBuffer.writeBytes(lengthByteBuffer);
+    grpcRequestBuffer.writeBytes(dataBytes);
+
+    channel.writeInbound(fullGrpcRequest(grpcRequestBuffer, streamId, true));
+
+    Response response = channel.readOutbound();
+    SegmentedData segmentedData = channel.readOutbound();
+
+    assertEquals(HttpResponseStatus.OK, response.status());
+    assertEquals(streamId, response.streamId());
+    assertEquals("application/grpc+proto", response.headers().get(HttpHeaderNames.CONTENT_TYPE));
+
+    assertEquals("13", Objects.requireNonNull(segmentedData.trailingHeaders()).get("grpc-status"));
+    assertTrue(segmentedData.endOfMessage());
+  }
+
+  private ByteBuf bufferFor(com.google.protobuf.GeneratedMessageV3 protoObject, boolean compressed) {
     byte[] dataBytes = protoObject.toByteArray();
     int length = dataBytes.length;
     byte[] lengthByteBuffer = ByteBuffer.allocate(4).putInt(length).array();
-    byte[] compressedByteBuffer = ByteBuffer.allocate(1).put((byte)0).array();
+    int compressedFlag = compressed ? 1 : 0;
+    byte[] compressedByteBuffer = ByteBuffer.allocate(1).put((byte)compressedFlag).array();
 
     ByteBuf grpcRequestBuffer = UnpooledByteBufAllocator.DEFAULT.buffer(length + 5, length + 5);
 
@@ -34,6 +257,10 @@ public class GrpcRequestHandlerTest extends Assert {
     grpcRequestBuffer.writeBytes(dataBytes);
 
     return grpcRequestBuffer;
+  }
+
+  private ByteBuf bufferFor(com.google.protobuf.GeneratedMessageV3 protoObject) {
+    return bufferFor(protoObject, false);
   }
 
   private <T extends GeneratedMessageV3> T protoObjectFor(ByteBuf originalBuffer, GrpcRequestParser<T> parser) {
@@ -49,13 +276,13 @@ public class GrpcRequestHandlerTest extends Assert {
     }
   }
 
-  private SegmentedRequestData fullGrpcRequest(ByteBuf grpcRequestBuffer, boolean endOfMessage) {
+  private SegmentedRequestData fullGrpcRequest(ByteBuf grpcRequestBuffer, int streamId, boolean endOfMessage) {
     Request request = DefaultSegmentedRequest
       .builder()
       .path("/")
       .method(HttpMethod.GET)
       .headers(new DefaultHeaders())
-      .streamId(1)
+      .streamId(streamId)
       .build();
     DefaultSegmentedData requestData = DefaultSegmentedData
       .builder()
@@ -64,49 +291,5 @@ public class GrpcRequestHandlerTest extends Assert {
       .build();
 
     return new SegmentedRequestData(request, requestData);
-  }
-
-  @Before
-  public void setUp() {
-    subject = new GrpcRequestHandler<HelloRequest, HelloReply>(HelloRequest::parseFrom, (HelloRequest request) -> {
-      return HelloReply.newBuilder().setMessage(responsePrefix + request.getName()).build();
-    });
-
-    channel = new EmbeddedChannel(new SimpleChannelInboundHandler<Request>() {
-      @Override
-      protected void channelRead0(ChannelHandlerContext ctx, Request request) throws Exception {
-        subject.handle(ctx, request, null);
-      }
-    });
-  }
-
-  @Test
-  public void testWholeRequest() {
-    HelloRequest grpcRequest = HelloRequest.newBuilder().setName("myName").build();
-    ByteBuf grpcRequestBuffer = bufferFor(grpcRequest);
-
-    SegmentedRequestData segmentedRequest = fullGrpcRequest(grpcRequestBuffer, true);
-    channel.writeInbound(segmentedRequest);
-
-    HelloReply actualReply = protoObjectFor(channel.readOutbound(), HelloReply::parseFrom);
-    HelloReply expectedReply = HelloReply.newBuilder().setMessage(responsePrefix + grpcRequest.getName()).build();
-    assertEquals(actualReply, expectedReply);
-  }
-
-  @Test
-  public void testSegmentedRequest() {
-    HelloRequest grpcRequest = HelloRequest.newBuilder().setName("myName").build();
-    ByteBuf grpcRequestBuffer = bufferFor(grpcRequest);
-
-    int middleIndex = grpcRequestBuffer.readableBytes() / 2;
-    ByteBuf firstHalf = grpcRequestBuffer.slice(0, middleIndex);
-    ByteBuf secondHalf = grpcRequestBuffer.slice(middleIndex, grpcRequestBuffer.readableBytes() - middleIndex);
-
-    channel.writeInbound(fullGrpcRequest(firstHalf, false));
-    channel.writeInbound(fullGrpcRequest(secondHalf, true));
-
-    HelloReply actualReply = protoObjectFor(channel.readOutbound(), HelloReply::parseFrom);
-    HelloReply expectedReply = HelloReply.newBuilder().setMessage(responsePrefix + grpcRequest.getName()).build();
-    assertEquals(actualReply, expectedReply);
   }
 }
