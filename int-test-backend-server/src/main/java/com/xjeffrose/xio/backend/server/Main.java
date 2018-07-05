5b4f81e54146eeb3280dda24ef642530ff0b5735
Cody Vandermyn <cody.vandermyn@nordstrom.com>
2018-07-05 10:40:10 -0700

    TACO-352 - use sparkjava as the http1.1 backend for the python int-tests

--- int-test-backend-server/src/main/java/com/xjeffrose/xio/backend/server/Main.java
+++ int-test-backend-server/src/main/java/com/xjeffrose/xio/backend/server/Main.java
@@ -1,53 +1,85 @@
 package com.xjeffrose.xio.backend.server;
 
 import com.codahale.metrics.health.HealthCheck;
-import com.nordstrom.xrpc.encoding.Encoder;
-import com.nordstrom.xrpc.server.Handler;
-import com.nordstrom.xrpc.server.RouteBuilder;
+import com.google.gson.Gson;
 import com.nordstrom.xrpc.server.Server;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import io.netty.buffer.ByteBuf;
-import io.netty.handler.codec.http.HttpMethod;
-import io.netty.handler.codec.http.HttpResponseStatus;
-import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import spark.Request;
+import spark.Response;
+import spark.resource.ClassPathResource;
+import spark.resource.Resource;
 
-import java.beans.ConstructorProperties;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
+
+import static spark.Spark.*;
 
 @Slf4j
 public class Main {
   public static void main(String args[]) throws Exception {
-    if (args.length < 2) {
-      throw new RuntimeException("please specify server 'port' and 'header-tag' arguments");
+    if (args.length < 3) {
+      throw new RuntimeException("please specify server 'port', 'header-tag' and 'h2' arguments");
     }
 
     // header-tag might be the ip address of this host or any other information you
     // would like to use to identify the traffic served up by this host
     final int port = Integer.parseInt(args[0]);
     final String name = args[1];
+    final Boolean h2Capable = Boolean.parseBoolean(args[2]);
 
-    Config config = ConfigFactory.load("application.conf");
-    Server server = new Server(config.getConfig("xrpc"), port);
-    Main.configure(server, name);
+    if (h2Capable) {
+      Config config = ConfigFactory.load("application.conf");
+      Server server = new Server(config.getConfig("xrpc"), port);
+      configureXrpc(server, name);
 
-    try {
-      // Fire away
-      server.listenAndServe();
-    } catch (IOException e) {
-      log.error("Failed to start people server", e);
+      try {
+        server.listenAndServe();
+      } catch (IOException e) {
+        log.error("Failed to start people server", e);
+      }
+
+      Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
+    } else {
+      setupSpark(port, name);
+      awaitInitialization();
+      System.out.println("Active Connections");
     }
-
-    Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
   }
 
-  public static void configure(Server server, String name) {
+  private static void setupSpark(int port, String name) throws Exception {
+    Resource keystore = new ClassPathResource("snakeoil.jks");
+    URL url = keystore.getURL();
+    String path = url.toString();
+    secure(path, "snakeoil", path, "snakeoil");
+    port(port);
+    get("/", (req, res) -> setupSparkResponse(name, req, res));
+    post("/", (req, res) -> setupSparkResponse(name, req, res));
+  }
+
+  private static Object setupSparkResponse(String name, Request req, Response res) {
+    res.type("application/json");
+    Optional<String> echoValue = Optional.ofNullable(req.headers("x-echo"));
+    Optional<String> optionalMethod = Optional.ofNullable(req.requestMethod());
+    String methodValue = "";
+    if (optionalMethod.isPresent()) {
+      methodValue = optionalMethod.get();
+    }
+    Optional<String> tagValue = Optional.ofNullable(name);
+    res.header("x-echo", echoValue.orElse("none"));
+    res.header("x-method", methodValue);
+    res.header("x-tag", tagValue.orElse("no tag"));
+    Kraken kraken = new Kraken("Release", "the Kraken");
+    return new Gson().toJson(kraken);
+  }
+
+  private static void configureXrpc(Server server, String name) {
     // Add handlers for routes
-    server.addRoutes(new KrakenRoutes(name));
+    server.addRoutes(new XrpcKrakenRoutes(name));
 
     // Add a service specific health check
     server.addHealthCheck(
@@ -59,43 +91,5 @@ public class Main {
           return Result.healthy();
         }
       });
-  }
-
-  private static class KrakenRoutes extends RouteBuilder {
-    KrakenRoutes(String name) {
-      Handler handler = (request) -> {
-        Kraken kraken = new Kraken("Release", "the Kraken");
-        final Encoder encoder = request.connectionContext().encoders().acceptedEncoder(request.acceptHeader());
-        ByteBuf buf = request.byteBuf();
-        ByteBuf encoded = encoder.encode(buf, request.acceptCharsetHeader(), kraken);
-
-        Optional<String> echoValue = Optional.ofNullable(request.header("x-echo")).map(CharSequence::toString);
-        Optional<HttpMethod> optionalMethod = request.method();
-        String methodValue = "";
-        if (optionalMethod.isPresent()) {
-          methodValue = optionalMethod.get().name();
-        }
-        Optional<String> tagValue = Optional.ofNullable(name);
-
-        Map<String, String> customHeaders = new HashMap<>();
-        customHeaders.put("x-echo", echoValue.orElse("none"));
-        customHeaders.put("x-method", methodValue);
-        customHeaders.put("x-tag", tagValue.orElse("no tag"));
-        return request.createResponse(HttpResponseStatus.OK, encoded, encoder.mediaType(), customHeaders);
-      };
-      get("/", handler);
-      post("/", handler);
-    }
-  }
-
-  private static class Kraken {
-    @Getter String title;
-    @Getter String description;
-
-    @ConstructorProperties({"title", "description"})
-    Kraken(String title, String description) {
-      this.title = title;
-      this.description = description;
-    }
   }
 }
