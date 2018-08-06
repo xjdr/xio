b10ef169a16ddb9151581fda8d2b45cf87a66edd
David Capino <david.capino@gmail.com>
2018-08-06 15:10:09 -0700

    dc/TACO-425 update the signature to use a throwing function

--- xio-core/src/main/java/com/xjeffrose/xio/config/RouteReloader.java
+++ xio-core/src/main/java/com/xjeffrose/xio/config/RouteReloader.java
@@ -13,7 +13,6 @@ import java.util.*;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 
