b87d2117002a619605bf62155d3bbd224363c216
Cody Vandermyn <codeman9@hotmail.com>
2018-09-06 10:03:26 -0700

    TACO-543 - add datadog support via an enum type in the configuration … (#299)
    * TACO-543 - add datadog support via an enum type in the configuration file.
    
    * TACO-543 - log the configured tracer type

--- xio-core/src/main/java/com/xjeffrose/xio/tracing/XioTracing.java
+++ xio-core/src/main/java/com/xjeffrose/xio/tracing/XioTracing.java
@@ -5,8 +5,10 @@ import brave.context.slf4j.MDCCurrentTraceContext;
 import brave.opentracing.BraveTracer;
 import brave.sampler.Sampler;
 import com.xjeffrose.xio.config.TracingConfig;
+import datadog.opentracing.DDTracer;
 import io.opentracing.Tracer;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
 import zipkin2.Span;
 import zipkin2.codec.Encoding;
@@ -16,6 +18,7 @@ import zipkin2.reporter.okhttp3.OkHttpClientBuilderFactory;
 import zipkin2.reporter.okhttp3.OkHttpSender;
 import zipkin2.reporter.okhttp3.OkHttpSenderBuilderFactory;
 
+@Slf4j
 public class XioTracing {
 
   private Tracer tracer;
@@ -23,12 +26,22 @@ public class XioTracing {
 
   public XioTracing(TracingConfig config) {
     name = config.getApplicationName();
-    String zipkinUrl = config.getZipkinUrl();
-    float samplingRate = config.getZipkinSamplingRate();
-    Tracing tracing = buildZipkinTracing(this.name, zipkinUrl, samplingRate);
-    if (tracing != null) {
-      tracer = BraveTracer.create(tracing);
+    TracingConfig.TracingType type = config.getType();
+
+    switch (type) {
+      case ZIPKIN:
+        String zipkinUrl = config.getZipkinUrl();
+        float samplingRate = config.getZipkinSamplingRate();
+        Tracing tracing = buildZipkinTracing(this.name, zipkinUrl, samplingRate);
+        if (tracing != null) {
+          tracer = BraveTracer.create(tracing);
+        }
+        break;
+      case DATADOG:
+        tracer = new DDTracer();
+        break;
     }
+    log.info("Configured tracer type: {}", type.toString());
   }
 
   public boolean enabled() {
