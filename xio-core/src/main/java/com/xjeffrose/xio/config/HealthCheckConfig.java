604bd7c8f103235a87e73c492a3e65c85d3d9fb2
David Capino <david.capino@gmail.com>
2018-09-21 15:24:28 -0700

    dc/TACO-597-update-healthcheckconfig adding port and tlsenabled in xio

--- xio-core/src/main/java/com/xjeffrose/xio/config/HealthCheckConfig.java
+++ xio-core/src/main/java/com/xjeffrose/xio/config/HealthCheckConfig.java
@@ -2,4 +2,6 @@ package com.xjeffrose.xio.config;
 
 public class HealthCheckConfig {
   public static final String healthCheckPath = "/health-check/";
+  public static final int healthCheckPort = 8000;
+  public static final boolean healthCheckTlsEnabled = false;
 }
