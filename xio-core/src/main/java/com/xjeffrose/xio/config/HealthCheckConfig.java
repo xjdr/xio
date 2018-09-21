356167ce12bf0e2ee9dbc51a0c5f7f9ec0b97223
David Capino <david.capino@gmail.com>
2018-09-21 15:27:23 -0700

    dc/TACO-597-update-healthcheckconfig adding host as well for default healthcheck

--- xio-core/src/main/java/com/xjeffrose/xio/config/HealthCheckConfig.java
+++ xio-core/src/main/java/com/xjeffrose/xio/config/HealthCheckConfig.java
@@ -3,5 +3,6 @@ package com.xjeffrose.xio.config;
 public class HealthCheckConfig {
   public static final String healthCheckPath = "/health-check/";
   public static final int healthCheckPort = 8000;
+  public static final String healthCheckHost = "127.0.0.1";
   public static final boolean healthCheckTlsEnabled = false;
 }
