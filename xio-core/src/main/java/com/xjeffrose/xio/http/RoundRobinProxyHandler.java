75e04b0b54cc699703cda08b25549cd292fc1bd7
Willard Kamp <manimaul@gmail.com>
2018-10-10 12:56:26 -0700

    wk/statistical_round_robin (#329)
    * wk/statistical_round_robin
    
    * keep standard round robin handler option

--- xio-core/src/main/java/com/xjeffrose/xio/http/RoundRobinProxyHandler.java
+++ xio-core/src/main/java/com/xjeffrose/xio/http/RoundRobinProxyHandler.java
@@ -7,18 +7,47 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.util.AttributeKey;
 import io.netty.util.collection.IntObjectHashMap;
 import io.netty.util.collection.IntObjectMap;
+import io.netty.util.internal.ThreadLocalRandom;
+import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 import lombok.val;
 
 public class RoundRobinProxyHandler extends ProxyHandler {
   private static final AttributeKey<IntObjectMap<Optional<ClientConfig>>> ROUND_ROBIN_KEY =
       AttributeKey.newInstance("xio_round_robin_key");
   private final AtomicInteger next = new AtomicInteger();
+  private final Supplier<Optional<ClientConfig>> computationFunction;
 
-  public RoundRobinProxyHandler(
+  public static RoundRobinProxyHandler createStatisticalRoundRobinHandler(
       ClientFactory factory, ProxyRouteConfig config, SocketAddressHelper addressHelper) {
+    return new RoundRobinProxyHandler(factory, config, addressHelper, ThreadLocalRandom::current);
+  }
+
+  public static RoundRobinProxyHandler createStandardRoundRobinHandler(
+      ClientFactory factory, ProxyRouteConfig config, SocketAddressHelper addressHelper) {
+    return new RoundRobinProxyHandler(factory, config, addressHelper, null);
+  }
+
+  private RoundRobinProxyHandler(
+      ClientFactory factory, ProxyRouteConfig config, SocketAddressHelper addressHelper) {
+    this(factory, config, addressHelper, ThreadLocalRandom::current);
+  }
+
+  @VisibleForTesting
+  RoundRobinProxyHandler(
+      ClientFactory factory,
+      ProxyRouteConfig config,
+      SocketAddressHelper addressHelper,
+      Supplier<Random> randomSupplier) {
     super(factory, config, addressHelper);
+    if (randomSupplier == null) {
+      this.computationFunction = this::computeRoundRobin;
+    } else {
+      this.computationFunction = () -> this.computeStatisticalRoundRobin(randomSupplier);
+    }
   }
 
   @Override
@@ -27,7 +56,7 @@ public class RoundRobinProxyHandler extends ProxyHandler {
     IntObjectMap<Optional<ClientConfig>> cachedClientConfig =
         ctx.channel().attr(ROUND_ROBIN_KEY).get();
     if (cachedClientConfig == null) {
-      cachedClientConfig = new IntObjectHashMap<Optional<ClientConfig>>();
+      cachedClientConfig = new IntObjectHashMap<>();
       ctx.channel().attr(ROUND_ROBIN_KEY).set(cachedClientConfig);
     }
     return getClientConfig(cachedClientConfig, request);
@@ -39,9 +68,9 @@ public class RoundRobinProxyHandler extends ProxyHandler {
     int hashKey = request.streamId();
     // we only do a fresh round robin if the request is a either a FULL HTTP REQUEST or the First Part of a Chunked Requets
     if (request.isFullMessage()) {
-      return computeRoundRobin(request);
+      return computationFunction.get();
     } else if (request.startOfMessage()) {
-      Optional<ClientConfig> newClientConfig = computeRoundRobin(request);
+      Optional<ClientConfig> newClientConfig = computationFunction.get();
       cachedClientConfig.put(hashKey, newClientConfig);
       return newClientConfig;
     } else if (request.endOfMessage()) {
@@ -51,7 +80,7 @@ public class RoundRobinProxyHandler extends ProxyHandler {
     }
   }
 
-  private Optional<ClientConfig> computeRoundRobin(Request request) {
+  private Optional<ClientConfig> computeRoundRobin() {
     int idx = next.getAndIncrement();
     // when we overflow from incrementing
     if (idx < 0) {
@@ -64,5 +93,15 @@ public class RoundRobinProxyHandler extends ProxyHandler {
       return Optional.of(clientConfigs.get(idx % clientConfigs.size()));
     }
     return Optional.empty();
+  }
+
+  private Optional<ClientConfig> computeStatisticalRoundRobin(Supplier<Random> randomSupplier) {
+    List<ClientConfig> clientConfigs = config.clientConfigs();
+    if (clientConfigs.size() > 0) {
+      int index = randomSupplier.get().nextInt(clientConfigs.size());
+      return Optional.of(clientConfigs.get(index));
+    } else {
+      return Optional.empty();
+    }
   }
 }
